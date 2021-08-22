a36cd4751e006888e898d3ac8acf1f8398f3d20b
Ross Mitchell <rossamitchell2609@yahoo.co.uk>
2021-08-22 04:05:12 +0100

    Working 'Start Instance' Feature

--- index.php
+++ index.php
@@ -1,25 +1,56 @@
 <?php
 
-if (file_exists(__DIR__ . '/.env')) {
-    // This is for locally running the project only, none of this is needed for production
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-    try {
-        $dotenv->required(['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'PASSWORD', 'INSTANCE_ID', 'RAM_IN_GB'])->notEmpty();
-        putenv("AWS_ACCESS_KEY_ID={$_ENV['AWS_ACCESS_KEY_ID']}");
-        putenv("AWS_SECRET_ACCESS_KEY={$_ENV['AWS_SECRET_ACCESS_KEY']}");
-        putenv("PASSWORD={$_ENV['PASSWORD']}");
-        putenv("INSTANCE_ID={$_ENV['INSTANCE_ID']}");
-        putenv("RAM_IN_GB={$_ENV['RAM_IN_GB']}");
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Aws\Ec2\Ec2Client;
+
+// This is for locally running the project only, none of this is needed for production
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+try {
+    putenv("PASSWORD={$_ENV['PASSWORD']}");
+    putenv("INSTANCE_ID={$_ENV['INSTANCE_ID']}");
+    putenv("AWS_KEY={$_ENV['AWS_KEY']}");
+    putenv("AWS_SECRET={$_ENV['AWS_SECRET']}");
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    die(getenv('PASSWORD'));
-    if ($_POST['password'] === 'CROC') {
-        echo "Password correct! :)";
+    if ($_POST['password'] === getenv('PASSWORD')) {
+        $action = 'START';
+
+        // Get AWS instance by ID
+        $ec2Client = new Aws\Ec2\Ec2Client([
+            'region' => 'eu-west-2',
+            'version' => '2016-11-15',
+            'credentials' => array(
+                'key' => getenv('AWS_KEY'),
+                'secret'  => getenv('AWS_SECRET'),
+              )
+        ]);
+
+        $instanceId = getenv('INSTANCE_ID');
+
+        $ids = [getenv('INSTANCE_ID')];
+        
+        if ($action == 'START') {
+            $result = $ec2Client->startInstances([
+                'InstanceIds' => $ids,
+            ]);
+        } else {
+            $result = $ec2Client->stopInstances([
+                'InstanceIds' => $ids,
+            ]);
+        }
+
+        var_dump($result);
+
+        // Start Instance and get IP
+        
+        // SSH into server using ip
+
     }
     else {
         echo "Password incorrect... :(";
@@ -34,17 +65,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link href="../resources/styles.css" rel="stylesheet" >
+    <link href="resources/styles.css" rel="stylesheet" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-    <title>Terraria Server</title>
+    <title>MC Terraria Server</title>
 </head>
 <body>
-    <h2 id="server--status">Server Status: Offline</h2>
-    <form method="POST">
-        <div id="server--controls">
-            <input class="form-control" type="password" name="password" id="password" placeholder="Password">
-            <input type="submit" value="Start">
-        </div>
-    </form>
+    <div class="container">
+        <h1>Mall Cops Terraria Server</h1>
+        <h2 id="server--status">Server Status: Offline</h2>
+        <form method="POST">
+            <div id="server--controls">
+                <input class="form-control" type="password" name="password" id="password" placeholder="Password">
+                <input type="submit" value="Start">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
